b92eac062da8c5f7f903dcfd5e4c8b74b15c2327
ggruszczynski <ggruszczynski@gmail.com>
2019-08-16 14:09:35 +0200

    shorter tests

--- hashmaker.sh
+++ hashmaker.sh
@@ -8,18 +8,11 @@ DIRNAME=$1
 
 if test -d $DIRNAME
 then
-	# cd $DIRNAME
     for i in $DIRNAME/*.vti
     do
     	echo " --- hashing $i --- "
     	sha1sum $i > "$i.sha1"
-    	#echo " --- launching job $i --- "
-		#log_name=$(echo $i | sed 's/$DIRNAME//g' | sed 's/xml/log/g')
-		# echo -e $log_name
-		#eval "CLB/$MODEL/main $i | tee $log_name"
-		# eval "CLB/$MODEL/main $i > $log_name 2>&1"
-		# sleep 1
-		# tsp "CLB/$MODEL/main $i"
+    	rm $i
     done
 else
 	echo directory doesnt exists
